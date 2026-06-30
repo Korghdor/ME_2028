0db39b2f248aec5f892c4dde3eca445954f41b7a
Korghdor <mahejekz@gmail.com>
2026-06-30 15:51:36 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.ME2028_SUPABASE = {
   url: "https://poztffgnnrcedjkekdky.supabase.co/rest/v1/",
-  anonKey: "WSTAW_TUTAJ_ANON_PUBLIC_KEY",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvenRmZmdubnJjZWRqa2VrZGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTgxMTUsImV4cCI6MjA5ODM5NDExNX0.Vt8wcrOtJX4I-tNjpq-2WaKwaUtEfQ_hZPbCkPblM7A",
 };
